698a44e5055192c29c93283a3d679d66e12eb3ba
Md.Ratul Uddin Ashraf <ratulshourov01@gmail.com>
2019-04-14 00:47:35 +0600

    After Seeding dummy data for product and review table

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,6 +4,11 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Model\Product::class, function (Faker $faker) {
     return [
+    	'name'=>$faker->word,
+    	'details'=>$faker->paragraph,
+    	'price'=>$faker->numberBetween(100,1000),
+    	'stock'=>$faker->numberBetween(10,1000),
+    	'discount'=>$faker->numberBetween(0,5),
         //
     ];
 });
--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -4,6 +4,12 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Model\Review::class, function (Faker $faker) {
     return [
+    	'product_id'=>function() {
+    		return App\Model\Product::all()->random();
+    	},
+    	'customer'=>$faker->word,
+    	'review'=>$faker->word,
+    	'star'=>$faker->numberBetween(0,5),
         //
     ];
 });
